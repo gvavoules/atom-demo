822e2dc719c9cc781340abb70cbab45264deef33
Greg Vavoules <gvavoules@gmail.com>
2020-07-06 13:31:46 -0400

    new

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-const message = 'Hello world';
+import { getCars } from './getCars';
+import { generateReport } from './generateReport';
 
-document.getElementById('message-element')
-  .textContent = message;
+document.getElementById('message-element').textContent = 'Loading...';
+
+getCars().then(cars => {
+  const message = generateReport(cars, 2000);
+  document.getElementById('message-element').textContent = message;
+});
